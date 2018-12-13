c91732a0fb605729a85d46b5d864fd3891f18fe7
Monette Fornaleza <monettefornaleza@gmail.com>
2018-12-13 15:01:25 +0800

    added Button_BootstrapColors

---
+++ Budirty/Button_BootstrapColors.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Windows.Forms;
+
+namespace Budirty
+{
+    public partial class Button_BootstrapColors : Button
+    {
+        public enum Type
+        {
+            Primary,
+            Danger,
+            Warning,
+            Success,
+            Info,
+            Light,
+            Secondary,
+            Dark
+        }
+
+        Color primary = Color.FromArgb(0, 123, 255);
+        Color danger = Color.FromArgb(220, 53, 69);
+        Color warning = Color.FromArgb(255, 193, 7);
+        Color success = Color.FromArgb(40, 167, 69);
+        Color info = Color.FromArgb(23, 162, 184);
+        Color light = Color.FromArgb(248, 249, 250);
+        Color secondary = Color.FromArgb(134, 142, 150);
+        Color dark = Color.FromArgb(52, 58, 64);
+
+        [Browsable(true), DefaultValue("All"), Description("Select what type of bootstrap button")]
+        [ListBindable(true), Editor(typeof(ComboBox), typeof(UITypeEditor))]
+        Type _type;
+
+        public Type Btn_Type
+        {
+            get { return _type; }
+            set { _type = value; typeChanged(); }
+        }
+
+        public event Action TypeChanged;
+
+        protected void typeChanged()
+        {
+            Action handler = TypeChanged;
+            DisplayDefault();
+            handler?.Invoke();
+        }
+
+        public Button_BootstrapColors()
+        {
+            FlatStyle = FlatStyle.Flat;
+            Font = new Font("Century Gothic", 9.75F, FontStyle.Regular);
+
+            DisplayDefault();
+
+            MouseHover += Btn_Primary_GotFocus;
+            MouseLeave += Btn_Primary_LostFocus;
+
+            GotFocus += Btn_Primary_GotFocus;
+            LostFocus += Btn_Primary_LostFocus;
+        }
+
+        private void Btn_Primary_LostFocus(object sender, System.EventArgs e)
+        {
+            DisplayDefault();
+        }
+
+        private void Btn_Primary_GotFocus(object sender, System.EventArgs e)
+        {
+            DisplayReverse();
+        }
+
+        private void DisplayDefault()
+        {
+            switch (_type)
+            {
+                case Type.Primary:
+                    ForeColor = primary;
+                    BackColor = light;
+                    break;
+                case Type.Danger:
+                    ForeColor = danger;
+                    BackColor = light;
+                    break;
+                case Type.Warning:
+                    ForeColor = warning;
+                    BackColor = light;
+                    break;
+                case Type.Success:
+                    ForeColor = success;
+                    BackColor = light;
+                    break;
+                case Type.Info:
+                    ForeColor = info;
+                    BackColor = light;
+                    break;
+                case Type.Light:
+                    ForeColor = dark;
+                    BackColor = light;
+                    break;
+                case Type.Secondary:
+                    ForeColor = secondary;
+                    BackColor = light;
+                    break;
+                case Type.Dark:
+                    ForeColor = light;
+                    BackColor = dark;
+                    break;
+            }
+        }
+
+        private void DisplayReverse()
+        {
+            switch (_type)
+            {
+                case Type.Primary:
+                    ForeColor = light;
+                    BackColor = primary;
+                    break;
+                case Type.Danger:
+                    ForeColor = light;
+                    BackColor = danger;
+                    break;
+                case Type.Warning:
+                    ForeColor = light;
+                    BackColor = warning;
+                    break;
+                case Type.Success:
+                    ForeColor = light;
+                    BackColor = success;
+                    break;
+                case Type.Info:
+                    ForeColor = light;
+                    BackColor = info;
+                    break;
+                case Type.Light:
+                    ForeColor = light;
+                    BackColor = dark;
+                    break;
+                case Type.Secondary:
+                    ForeColor = light;
+                    BackColor = secondary;
+                    break;
+                case Type.Dark:
+                    ForeColor = dark;
+                    BackColor = light;
+                    break;
+            }
+        }
+    }
+}
--- Budirty/Form1.Designer.cs
+++ Budirty/Form1.Designer.cs
@@ -28,12 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_BootstrapColors1 = new Budirty.Button_BootstrapColors();
+            this.button_BootstrapColors2 = new Budirty.Button_BootstrapColors();
+            this.button_BootstrapColors3 = new Budirty.Button_BootstrapColors();
+            this.button_BootstrapColors4 = new Budirty.Button_BootstrapColors();
+            this.button_BootstrapColors5 = new Budirty.Button_BootstrapColors();
+            this.button_BootstrapColors6 = new Budirty.Button_BootstrapColors();
+            this.button_BootstrapColors7 = new Budirty.Button_BootstrapColors();
+            this.button_BootstrapColors8 = new Budirty.Button_BootstrapColors();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_BootstrapColors1
+            // 
+            this.button_BootstrapColors1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors1.Btn_Type = Budirty.Button_BootstrapColors.Type.Primary;
+            this.button_BootstrapColors1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
+            this.button_BootstrapColors1.Location = new System.Drawing.Point(12, 31);
+            this.button_BootstrapColors1.Name = "button_BootstrapColors1";
+            this.button_BootstrapColors1.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors1.TabIndex = 0;
+            this.button_BootstrapColors1.Text = "primary";
+            this.button_BootstrapColors1.UseVisualStyleBackColor = false;
+            // 
+            // button_BootstrapColors2
+            // 
+            this.button_BootstrapColors2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors2.Btn_Type = Budirty.Button_BootstrapColors.Type.Danger;
+            this.button_BootstrapColors2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(53)))), ((int)(((byte)(69)))));
+            this.button_BootstrapColors2.Location = new System.Drawing.Point(12, 65);
+            this.button_BootstrapColors2.Name = "button_BootstrapColors2";
+            this.button_BootstrapColors2.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors2.TabIndex = 1;
+            this.button_BootstrapColors2.Text = "danger";
+            this.button_BootstrapColors2.UseVisualStyleBackColor = false;
+            // 
+            // button_BootstrapColors3
+            // 
+            this.button_BootstrapColors3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors3.Btn_Type = Budirty.Button_BootstrapColors.Type.Warning;
+            this.button_BootstrapColors3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(193)))), ((int)(((byte)(7)))));
+            this.button_BootstrapColors3.Location = new System.Drawing.Point(12, 99);
+            this.button_BootstrapColors3.Name = "button_BootstrapColors3";
+            this.button_BootstrapColors3.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors3.TabIndex = 2;
+            this.button_BootstrapColors3.Text = "warning";
+            this.button_BootstrapColors3.UseVisualStyleBackColor = false;
+            // 
+            // button_BootstrapColors4
+            // 
+            this.button_BootstrapColors4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors4.Btn_Type = Budirty.Button_BootstrapColors.Type.Success;
+            this.button_BootstrapColors4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors4.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(167)))), ((int)(((byte)(69)))));
+            this.button_BootstrapColors4.Location = new System.Drawing.Point(12, 135);
+            this.button_BootstrapColors4.Name = "button_BootstrapColors4";
+            this.button_BootstrapColors4.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors4.TabIndex = 3;
+            this.button_BootstrapColors4.Text = "success";
+            this.button_BootstrapColors4.UseVisualStyleBackColor = false;
+            // 
+            // button_BootstrapColors5
+            // 
+            this.button_BootstrapColors5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors5.Btn_Type = Budirty.Button_BootstrapColors.Type.Info;
+            this.button_BootstrapColors5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors5.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(162)))), ((int)(((byte)(184)))));
+            this.button_BootstrapColors5.Location = new System.Drawing.Point(12, 169);
+            this.button_BootstrapColors5.Name = "button_BootstrapColors5";
+            this.button_BootstrapColors5.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors5.TabIndex = 4;
+            this.button_BootstrapColors5.Text = "info";
+            this.button_BootstrapColors5.UseVisualStyleBackColor = false;
+            // 
+            // button_BootstrapColors6
+            // 
+            this.button_BootstrapColors6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors6.Btn_Type = Budirty.Button_BootstrapColors.Type.Light;
+            this.button_BootstrapColors6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors6.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.button_BootstrapColors6.Location = new System.Drawing.Point(12, 203);
+            this.button_BootstrapColors6.Name = "button_BootstrapColors6";
+            this.button_BootstrapColors6.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors6.TabIndex = 5;
+            this.button_BootstrapColors6.Text = "light";
+            this.button_BootstrapColors6.UseVisualStyleBackColor = false;
+            // 
+            // button_BootstrapColors7
+            // 
+            this.button_BootstrapColors7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors7.Btn_Type = Budirty.Button_BootstrapColors.Type.Secondary;
+            this.button_BootstrapColors7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors7.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(142)))), ((int)(((byte)(150)))));
+            this.button_BootstrapColors7.Location = new System.Drawing.Point(12, 237);
+            this.button_BootstrapColors7.Name = "button_BootstrapColors7";
+            this.button_BootstrapColors7.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors7.TabIndex = 6;
+            this.button_BootstrapColors7.Text = "secondary";
+            this.button_BootstrapColors7.UseVisualStyleBackColor = false;
+            // 
+            // button_BootstrapColors8
+            // 
+            this.button_BootstrapColors8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.button_BootstrapColors8.Btn_Type = Budirty.Button_BootstrapColors.Type.Dark;
+            this.button_BootstrapColors8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BootstrapColors8.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button_BootstrapColors8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.button_BootstrapColors8.Location = new System.Drawing.Point(12, 271);
+            this.button_BootstrapColors8.Name = "button_BootstrapColors8";
+            this.button_BootstrapColors8.Size = new System.Drawing.Size(260, 28);
+            this.button_BootstrapColors8.TabIndex = 7;
+            this.button_BootstrapColors8.Text = "dark";
+            this.button_BootstrapColors8.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Sample usage of Button_BootstrapColors :\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_BootstrapColors8);
+            this.Controls.Add(this.button_BootstrapColors7);
+            this.Controls.Add(this.button_BootstrapColors6);
+            this.Controls.Add(this.button_BootstrapColors5);
+            this.Controls.Add(this.button_BootstrapColors4);
+            this.Controls.Add(this.button_BootstrapColors3);
+            this.Controls.Add(this.button_BootstrapColors2);
+            this.Controls.Add(this.button_BootstrapColors1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button_BootstrapColors button_BootstrapColors1;
+        private Button_BootstrapColors button_BootstrapColors2;
+        private Button_BootstrapColors button_BootstrapColors3;
+        private Button_BootstrapColors button_BootstrapColors4;
+        private Button_BootstrapColors button_BootstrapColors5;
+        private Button_BootstrapColors button_BootstrapColors6;
+        private Button_BootstrapColors button_BootstrapColors7;
+        private Button_BootstrapColors button_BootstrapColors8;
+        private System.Windows.Forms.Label label1;
     }
 }
 
